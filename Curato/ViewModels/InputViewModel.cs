62b814753186c78d36d334e4a8fada4558f463c1
sungwoo106 <sungwoo100604@gmail.com>
2025-07-21 12:49:34 +0700

    Add companion chooser to search page

--- Curato/ViewModels/InputViewModel.cs
+++ Curato/ViewModels/InputViewModel.cs
@@ -1,6 +1,10 @@
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
 using System.Windows.Input;
 using Curato.Models;
 using Curato.Helpers;
@@ -15,12 +19,37 @@ namespace Curato.ViewModels
     {
         public ObservableCollection<PopularPlace> PopularPlaces { get; set; }
 
+        public ObservableCollection<string> CompanionTypes { get; } = new ObservableCollection<string>();
+
+        private string? _selectedCompanion;
+        public string? SelectedCompanion
+        {
+            get => _selectedCompanion;
+            set
+            {
+                if (_selectedCompanion != value)
+                {
+                    _selectedCompanion = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(CompanionButtonText));
+                    OnPropertyChanged(nameof(CompanionSelected));
+                }
+            }
+        }
+
+        public string CompanionButtonText => string.IsNullOrEmpty(SelectedCompanion) ? "Companion" : SelectedCompanion;
+
+        public bool CompanionSelected => !string.IsNullOrEmpty(SelectedCompanion);
+
         public string LocationQuery { get; set; } = "Search Location";
 
         public ICommand GeneratePlanCommand { get; }
 
         public InputViewModel()
         {
+            foreach (var ct in FetchCompanionTypes())
+                CompanionTypes.Add(ct);
+
             PopularPlaces = new ObservableCollection<PopularPlace>
             {
                 new PopularPlace
@@ -69,6 +98,32 @@ namespace Curato.ViewModels
             // For now: simple placeholder action
             System.Diagnostics.Debug.WriteLine("Generate Plan Clicked");
         }
+
+        private IEnumerable<string> FetchCompanionTypes()
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "python3",
+                    Arguments = "-c \"import json, constants; print(json.dumps(constants.COMPANION_TYPES))\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using var process = Process.Start(psi)!;
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                return JsonSerializer.Deserialize<List<string>>(output) ?? new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to load companion types: {ex}");
+                return new List<string> { "solo", "couple", "friends", "family" };
+            }
+        }
+
 
         public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
--- Curato/Views/SearchPage.xaml.cs
+++ Curato/Views/SearchPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using Curato.ViewModels;
 
@@ -9,6 +11,23 @@ namespace Curato.Views
         {
             InitializeComponent();
             this.DataContext = new InputViewModel();
+        }
+
+        private void CompanionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is not InputViewModel vm)
+                return;
+
+            var menu = new ContextMenu();
+            foreach (string type in vm.CompanionTypes)
+            {
+                var item = new MenuItem { Header = type };
+                item.Click += (s, _) => { vm.SelectedCompanion = type; };
+                menu.Items.Add(item);
+            }
+
+            menu.PlacementTarget = sender as UIElement;
+            menu.IsOpen = true;
         }
     }
 }
