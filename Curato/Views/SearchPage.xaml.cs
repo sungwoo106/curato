20fcaa5accd1ab5998ef7aa2d25837d1d3463da5
sungwoo106 <sungwoo100604@gmail.com>
2025-07-21 15:58:14 +0700

    Added using reference

--- Curato/Views/SearchPage.xaml.cs
+++ Curato/Views/SearchPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Data;
 using Curato.ViewModels;
 
 namespace Curato.Views
