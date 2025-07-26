95e3bc4199d0be1e7b34e2bc9f226fad2f7b0d58
sungwoo106 <sungwoo100604@gmail.com>
2025-07-26 16:49:49 +0900

    Use System.Text namespace in SearchPage

--- Curato/Views/SearchPage.xaml.cs
+++ Curato/Views/SearchPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
+using System.Text;
 using System.Text.Json;
 using System.Linq;
 using System.Windows;
