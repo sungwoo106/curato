d9bc571fbd10438e6e6494ab32d342e71730bbd2
sungwoo106 <sungwoo100604@gmail.com>
2025-07-26 09:11:07 +0900

    Embed Kakao map in WebView2

--- Curato/Views/OutputPage.xaml.cs
+++ Curato/Views/OutputPage.xaml.cs
@@ -1,8 +1,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
-
-
+using System.Globalization;
+using System.IO;
 
 namespace Curato.Views
 {
@@ -12,6 +12,8 @@ namespace Curato.Views
         {
             InitializeComponent();
             this.DataContext = AppState.SharedTripPlan;
+
+            this.Loaded += OutputPage_Loaded;
 
             // Load the emotional narrative into the TextBlock
             var plan = AppState.SharedTripPlan;
@@ -28,6 +30,40 @@ namespace Curato.Views
                     EmotionalItineraryTextBlock.Inlines.Add(new LineBreak());
                     EmotionalItineraryTextBlock.Inlines.Add(new LineBreak());
                 }
+            }
+        }
+
+        private async void OutputPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            double lat = 37.5665; // default to Seoul
+            double lng = 126.9780;
+
+            var coords = AppState.SharedInputViewModel.SelectedLocationCoordinates;
+            if (coords.HasValue)
+            {
+                lat = coords.Value.Latitude;
+                lng = coords.Value.Longitude;
+            }
+
+            string apiKey = Environment.GetEnvironmentVariable("KAKAO_MAP_API_KEY") ?? "";
+
+            try
+            {
+                var htmlPath = Path.Combine(AppContext.BaseDirectory, "Resources", "Html", "map_template.html");
+                if (File.Exists(htmlPath))
+                {
+                    string html = File.ReadAllText(htmlPath)
+                        .Replace("{{API_KEY}}", apiKey)
+                        .Replace("{{LAT}}", lat.ToString(CultureInfo.InvariantCulture))
+                        .Replace("{{LNG}}", lng.ToString(CultureInfo.InvariantCulture));
+
+                    await MapWebView.EnsureCoreWebView2Async();
+                    MapWebView.NavigateToString(html);
+                }
+            }
+            catch
+            {
+                // ignore map errors
             }
         }
 
