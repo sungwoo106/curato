b862560932ca2fcddbb4fc07a793ead35495219a
sungwoo106 <sungwoo100604@gmail.com>
2025-08-20 22:59:10 +0900

    Fix Popup compilation error by adding System.Windows.Controls.Primitives using directive

--- Curato/Views/SearchPage.xaml.cs
+++ Curato/Views/SearchPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
